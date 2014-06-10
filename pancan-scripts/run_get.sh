0f85066ca3fc468f571a48349fed1fe347120da5
Joseph Lugo <joseph.r.lugo@gmail.com>
2014-06-10 17:09:49 -0400

    Update run_get.sh

--- pancan-scripts/run_get.sh
+++ pancan-scripts/run_get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Current_date=$(date +"%Y_%m_%d")
-cd ~/git/public-workflows/decider-bwa-pancancer/results
+cd ~/ubuntu/gitroot/pancancer-info/pancan-scripts/results
 mv all_sheets.txt all_sheets_"$Current_date".txt
 mv defiles.txt defiles_"$Current_date".txt
 mv esadukfiles.txt esadukfiles_"$Current_date".txt
@@ -11,7 +11,7 @@ mv sgfiles.txt sgfiles_"$Current_date".txt
 mv jpfiles.txt jpfiles_"$Current_date".txt
 mv brcaukfiles.txt brcaukfiles_"$Current_date".txt
 mv *.txt ../old_results
-perl `find ~/git -name get_spreadsheets.pl`
+perl `find ~/gitroot -name get_spreadsheets.pl`
 cat *.txt > all_sheets.txt
 cat *-DE*.txt > defiles.txt
 cat *ESAD-UK*.txt > esadukfiles.txt
@@ -20,10 +20,9 @@ cat *-ES*.txt > esfiles.txt
 cat *-SG*.txt > sgfiles.txt
 cat *-JP*.txt > jpfiles.txt
 cat *BRCA-UK*.txt > brcaukfiles.txt
-cd /usr/lib/cgi-bin
-./update_data
-cd /home/jlugo/Documents/perl-stuff/
+cd ~/ubuntu/gitroot/pancancer-info/pancan-scripts/map-data
+perl generate_upload_info.pl
 perl generate_upload_info.pl > out.csv
 perl arc_read.pl
 perl bubble_read.pl
-cd ~/git/public-workflows/decider-bwa-pancancer/results
+cd ~/ubuntu/gitroot/pancancer-info/pancan-scripts
